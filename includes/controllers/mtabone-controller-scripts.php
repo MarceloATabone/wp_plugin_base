0dcbba58be48e6769b64522178d617b65469ea4d
Marcelo A Tabone <marceloatabone@hotmail.com>
2021-11-10 19:35:05 -0300

    Create Load Libs

--- includes/controllers/mtabone-controller-scripts.php
+++ includes/controllers/mtabone-controller-scripts.php
@@ -1,7 +1,8 @@
 <?php
 
     function mtabone_loadLibs(){
-    
+        //wp_register_script( 'mtabone_bootstrap_lib', plugins_url( '../../includes/libs/bootstrap/bootstrap.js', __FILE__ ),array('jquery'),false,true );
+        //wp_enqueue_script( 'mtabone_bootstrap_lib');
     }
     function mtabone_loadCss() {
         wp_enqueue_style('mtabone-style-css', plugins_url('../css/mtabone-style.css', __FILE__));
