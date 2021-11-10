76b35f8c0436a960e70d46b4c54563c01c156d1c
Marcelo A Tabone <marceloatabone@hotmail.com>
2021-11-10 20:30:50 -0300

    Create generci tables for insert and create

--- includes/activation/mtabone-createDB.php
+++ includes/activation/mtabone-createDB.php
@@ -1,4 +1,25 @@
 <?php
     function mtabone_createTablesDB(){
-        
+        mtabone_defineConsts();
+        // mtabone_createTableGeneric();
+    }
+
+    function mtabone_defineConsts(){
+        global $wpdb;
+        $DB_NAME = $wpdb->prefix;
+        $mtabone_charsetCollate = $wpdb->get_charset_collate();
+        define('MTABONE_TABLEGeneric', $DB_NAME."mtabone_generic" );
+    }
+
+    function mtabone_createTableGeneric(){
+        $sql = 
+        "
+            CREATE TABLE IF NOT EXISTS ".MTABONE_TABLEGeneric." (
+                `generic_id` INT NOT NULL AUTO_INCREMENT,
+                `generic_name` VARCHAR(80) NOT NULL,
+            )$mtabone_charsetCollate;
+        ";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
     }
