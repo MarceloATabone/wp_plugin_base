30d2ed4ea38162e607b6b26de51aa27969967e62
Marcelo A Tabone <marceloatabone@hotmail.com>
2021-11-10 19:29:06 -0300

    Create css style Create load CSS

---
+++ includes/controllers/mtabone-controller-scripts.php
@@ -0,0 +1,11 @@
+<?php
+
+    function mtabone_loadLibs(){
+    
+    }
+    function mtabone_loadCss() {
+        wp_enqueue_style('mtabone-style-css', plugins_url('../css/mtabone-style.css', __FILE__));
+    }
+    function mtabone_loadScripts() {
+
+    }
